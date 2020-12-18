72ffb2a4f8f4d99cd4a87e462c4d9a50e4baefb3
byynaryy <endeki@gmail.com>
2020-12-18 13:48:22 +0100

    Initialize the the state of the component by setting 'this.state' in its constructor

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: null, };
+  }
+
   render() {
     return (
       <button className="square" onClick={() => alert("clicked")}>
